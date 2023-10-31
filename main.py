60ef1149689e19b2d69f7d47383d3a60d97db428
Jake Coughlan <jakecoughlan2004@gmail.com>
2023-10-31 15:35:21 +0000

    Flask app creating moved to __init__

--- main.py
+++ main.py
@@ -9,15 +9,7 @@ from os import path
 
 
 
-app = Flask(__name__, template_folder='e-voting/website/templates')
-
-
-
-@app.route("/")
-def home():
-    return render_template("vote.html")
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- website/views.py
+++ website/views.py
@@ -1,8 +1,12 @@
 from flask import Flask, Blueprint, request, redirect, url_for, render_template
 from website import views
 
+
 app = Flask(__name__)
 
 
 views = Blueprint('views', __name__)
 
+@app.route("/")
+def home():
+    return render_template("Proto1.html")
